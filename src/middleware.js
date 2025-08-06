a149d371a0d4e915fe9d06e7126725583ebfbbdb
TkWarrior <talibulhaque369@gmail.com>
2025-08-06 06:43:38 +0530

    added rate limiting and middleware

---
+++ src/middleware.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { rateLimit } from "./app/utils/rateLimit"
+
+export async function middleware(request){
+    const allowedOrigins = ["https://localhoast:3000","https://abc.com"];
+
+    if(request.method == 'GET'){
+
+        const orgin = request.headers.get('orgin');
+
+        if(allowedOrigins.includes(orgin)){
+            return NextResponse.json({message : "CORS BLOCKED"},{status : 403});
+        }
+
+        let ip = request.ip || request.headers.get("x-forwarded-for") || 'unknown'
+
+        const {limit , remaining ,reset} = await rateLimit.limit(ip);
+        console.log(limit ,remaining , "limit and remaining");
+        if(remaining === 0 ){
+            return NextResponse.json({message : "You've reached api limit"} , {status : 429});
+        }
+        return NextResponse.next();
+    }
+}
+
+export const config =  {
+    matcher : '/api/v1/:path'
+}
